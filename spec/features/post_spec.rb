363e02c80590d9f684b36370f07a6dbab56a29bf
Paulo Renato Castro <leitedecastro@gmail.com>
2017-10-25 06:13:48 +0000

    Implement index view with posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,27 +1,38 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create(
+      email: "test@example.com",
+      password: "asdfasdf", password_confirmation: "asdfasdf",
+      first_name: "Paulo", last_name: "Castro"
+    )
+    login_as(user, scope: :user)
+  end
   
   describe 'index' do
+    before do
+      visit posts_path
+    end
     
     it 'can be reached successfully' do
-      visit posts_path
       expect(page.status_code).to eq(200)
     end
     
     it "has a title of Posts" do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
     
+    it "has a list of Posts" do
+      post1 = Post.create(date: Date.today, rationale: "Post1")
+      post2 = Post.create(date: Date.today, rationale: "Post2")
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
+    end
   end
   
   describe "creation" do
     before do
-      user = User.create(email: "test@example.com", password: "asdfasdf",
-        password_confirmation: "asdfasdf", first_name: "Paulo",
-        last_name: "Castro")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
     
@@ -45,7 +56,5 @@ describe 'navigate' do
       
       expect(User.last.posts.last.rationale).to eq("User Association")
     end
-    
   end
-  
 end
