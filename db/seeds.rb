2097ea8974714373712431ed6c1ff4a516a5dbe0
Paulo Renato Castro <leitedecastro@gmail.com>
2017-10-25 06:13:22 +0000

    Update seed file to include user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
+@user = User.create(
+  email: "test@example.com",
+  password: "asdfasdf", password_confirmation: "asdfasdf",
+  first_name: "Paulo", last_name: "Castro"
+)
+
+puts "1 user created"
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(
+    date: Date.today,
+    rationale: "#{post} rationale content",
+    user_id: @user.id
+  )
 end
 
 puts "100 Post have been created"
