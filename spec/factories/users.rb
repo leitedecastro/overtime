ae8a03528697e9a9c9b22ae70145252d0580349a
Paulo Renato Castro <leitedecastro@gmail.com>
2017-10-25 15:28:36 +0000

    Install FactoryGirl for fixtures and create user and post factories

---
+++ spec/factories/users.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+  sequence :email do |n|
+    "test#{n}@example.com"
+  end
+  
+  factory :user do
+    email { generate :email }
+    password "asdfasdf"
+    password_confirmation "asdfasdf"
+    first_name "John"
+    last_name "Doe"
+  end
+  
+  factory :admin_user, class: "AdminUser" do
+    email { generate :email }
+    password "asdfasdf"
+    password_confirmation "asdfasdf"
+    first_name "Paulo"
+    last_name "Castro"
+  end
+end
