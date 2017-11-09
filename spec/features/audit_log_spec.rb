47c9aa358d5b6353a3c044ad017016854d72de37
Paulo Renato Castro <leitedecastro@gmail.com>
2017-11-09 22:11:46 +0800

    Add AuditLog#index authorization

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -20,6 +20,13 @@ describe 'AuditLog feature' do
     end
 
     it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, scope: :user)
+
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
