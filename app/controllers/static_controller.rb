274875bfe21da3232f6d13aed0f34af1e8ab55cd
Paulo Renato Castro <leitedecastro@gmail.com>
2017-11-14 23:19:50 +0800

    Add style for pending audit approvals

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,12 +1,12 @@
 class StaticController < ApplicationController
 
   def homepage
-    # TODO: look for a way to do this inside User class 
+    # TODO: look for a way to do this inside User class
     if admin_types.include?(current_user.type)
       @pending_approvals = Post.submitted
       @recent_audit_items = AuditLog.last(10)
     else
-      # something else
+      @pending_audit_confirmations = current_user.audit_logs
     end
   end
 
