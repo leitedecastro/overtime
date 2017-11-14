1fb5bc70c068389ad7c77f48097ce416ab3ce0fb
Paulo Renato Castro <leitedecastro@gmail.com>
2017-11-14 19:43:20 +0800

    Create skeleton employee homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,13 @@
 class StaticController < ApplicationController
 
   def homepage
-    @pending_approvals = Post.where(status: "submitted")
-    @recent_audit_items = AuditLog.last(10)
+    # TODO: look for a way to do this inside User class 
+    if admin_types.include?(current_user.type)
+      @pending_approvals = Post.submitted
+      @recent_audit_items = AuditLog.last(10)
+    else
+      # something else
+    end
   end
 
 end
