ce5e2acf989a1647d4d8bd009219688455aeea7a
Paulo Renato Castro <leitedecastro@gmail.com>
2017-11-15 11:41:18 +0800

    Add pending audit logs confirmation buttons

--- app/policies/audit_log_policy.rb
+++ app/policies/audit_log_policy.rb
@@ -2,4 +2,8 @@ class AuditLogPolicy < ApplicationPolicy
   def index?
     admin_types.include?(user.type)
   end
+
+  def confirm?
+    record.user.id == user.id
+  end
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,23 +1,38 @@
 require 'rails_helper'
 
 describe 'Homepage' do
-  before do
-    @post = FactoryGirl.create(:post)
-    @admin_user = FactoryGirl.create(:admin_user)
-    login_as(@admin_user, scope: :user)
+  describe 'Admin Dashboard' do
+    before do
+      @post = FactoryGirl.create(:post)
+      @admin_user = FactoryGirl.create(:admin_user)
+      login_as(@admin_user, scope: :user)
+    end
+
+    it 'allows the admin to approve posts from the homepage' do
+      visit root_path
+      click_link("approve_#{@post.id}")
+
+      expect(@post.reload.status).to eq("approved")
+    end
+
+    it 'prevent nonusers to approve posts' do
+      login_as(@post.user, scope: :user)
+      visit approve_post_path(@post)
+
+      expect(@post.reload.status).to_not eq("approved")
+    end
   end
 
-  it 'allows the admin to approve posts from the homepage' do
-    visit root_path
-    click_link("approve_#{@post.id}")
+  describe 'Employee Dashboard' do
+    it "allow the employee to change the audit log status from the homepage" do
+      user = FactoryGirl.create(:user)
+      audit_log = FactoryGirl.create(:audit_log, user: user)
+      login_as(user, scope: :user)
 
-    expect(@post.reload.status).to eq("approved")
-  end
+      visit root_path
+      click_link("confirm_#{audit_log.id}")
 
-  it 'prevent nonusers to approve posts' do
-    login_as(@post.user, scope: :user)
-    visit approve_post_path(@post)
-
-    expect(@post.reload.status).to_not eq("approved")
+      expect(audit_log.reload.status).to eq("confirmed")
+    end
   end
 end
