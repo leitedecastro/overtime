b95656b943356292a5a947f1e385516a33924ac6
Paulo Renato Castro <leitedecastro@gmail.com>
2017-11-16 11:11:12 +0800

    Update nav to show items accordingly to user type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,14 @@ module ApplicationHelper
     ['AdminUser']
   end
 
+  def admin?
+    admin_types.include?(current_user.type)
+  end
+
+  def employee?
+    current_user.type == "Employee"
+  end
+
   def active?(path)
     "active" if current_page?(path)
   end
